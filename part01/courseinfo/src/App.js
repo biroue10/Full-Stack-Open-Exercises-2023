621a27e7d044ae9a2e011c003b5b603ec8e95d17
biroue isaac <biroueisaac@gmail.com>
2023-05-10 03:07:18 +0100

    Exercise 1

---
+++ part01/courseinfo/src/App.js
@@ -0,0 +1,54 @@
+const Header = (props) => {
+  console.log(props)
+  return (
+    <>
+      {props.course}
+    </>
+  )
+}
+const Part = (props) => {
+  console.log(props)
+  return (
+    <>
+      <p>
+        {props.part} {props.exercises}
+      </p>
+    </>
+  )
+}
+const Content = (props) => {
+  return (
+    <>
+      <Part part={props.part1} exercises={props.exercises1} />
+      <Part part={props.part2} exercises={props.exercises2} />
+      <Part part={props.part3} exercises={props.exercises3} />
+    </>
+  )
+}
+const Total = (props) => {
+  return (
+    <>
+      <p>Number of exercise {props.sum}</p>
+    </>
+  )
+}
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+  const sum = exercises1 + exercises2 + exercises3
+  return (
+    <div>
+      <h1><Header course={course} /></h1>
+      <Content part1={part1} exercises1={exercises1} />
+      <Content part2={part2} exercises2={exercises2} />
+      <Content part3={part3} exercises3={exercises3} />
+      <Total sum={sum} />
+    </div>
+  )
+}
+export default App
