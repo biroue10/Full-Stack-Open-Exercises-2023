5195a1aa6e109a519b2b66e471e4319599565d5c
biroue isaac <biroueisaac@gmail.com>
2023-05-11 11:56:40 +0100

    Step 1 completed for the anecdotes project

--- part01/anecdotes/src/App.js
+++ part01/anecdotes/src/App.js
@@ -1,8 +1,37 @@
-function App() {
+import { useState } from 'react'
+
+const Button = (props) => {
   return (
-    <div className="App">
+    <div>
+      <button onClick={props.handleClick}>{props.text}</button>
     </div>
-  );
+  )
+}
+const App = () => {
+  const nextAnecdote = () => {
+    setSelected(Math.floor(Math.random() * anecdotes.length))
+    console.log(setSelected(Math.floor(Math.random() * anecdotes.length)))
+  }
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+
+  const [selected, setSelected] = useState(0)
+  console.log(selected)
+  return (
+    <div>
+      <p>{anecdotes[selected]}</p>
+      <Button handleClick={nextAnecdote} text="Next Anecdote" />
+    </div>
+
+  )
 }
 
-export default App;
+export default App
