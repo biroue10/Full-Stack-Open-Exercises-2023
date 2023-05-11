1bf38192be0966520a46948f2010db399b84074c
biroue isaac <biroueisaac@gmail.com>
2023-05-11 10:36:56 +0100

    Last steps of the project

--- part01/unicafe/src/App.js
+++ part01/unicafe/src/App.js
@@ -1,23 +1,65 @@
 import { useState } from 'react'
-
+const StaticticLine = (props) => {
+  return (
+    <>
+      <h1>give feedback</h1>
+      <table>
+        <tbody>
+          <tr><td>{props.bien}</td><td>{props.good}</td></tr>
+          <tr><td>{props.neutre}</td><td>{props.neutral}</td></tr>
+          <tr><td>{props.mauvais}</td><td>{props.bad}</td></tr>
+          <tr><td>{props.tous}</td><td>{(props.good + props.bad + props.neutral)}</td></tr>
+          <tr><td>{props.moyenne}</td><td>{(props.good + props.bad + props.neutral) / 3}</td></tr>
+          <tr><td>{props.positif}</td><td>{(props.good * 10) / 100}%</td></tr>
+        </tbody>
+      </table>
+    </>
+  )
+}
+const Button = (props) => {
+  return (
+    <>
+      <button onClick={props.fonction}>{props.text}</button>
+    </>
+  )
+}
+const Statictics = (props) => {
+  if (props.bad > 0 || props.neutral > 0 || props.good > 0) {
+    return (
+      <>
+        <StaticticLine
+          bien="Good"
+          good={props.good}
+          neutre="Neutral"
+          neutral={props.neutral}
+          mauvais="Bad" bad={props.bad}
+          tous="All"
+          moyenne="Average"
+          positif="Positive"
+        />
+      </>
+    )
+  }
+  return (
+    <>
+      <h1>give feedback</h1>
+      <p>No feedback given</p>
+    </>
+  )
+}
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-
   return (
     <div>
-      <h1>give feedback</h1>
-      <button onClick={() => setGood(good + 1)}>good</button>
-      <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
-      <button onClick={() => setBad(bad + 1)}>bad</button>
-      <h2>statistics</h2>
-      <div>good {good} </div>
-      <div>neutral {neutral}</div>
-      <div>bad {bad}</div>
+      <Button text="Good" fonction={() => setGood(good + 1)} />
+      <Button text="Neutral" fonction={() => setNeutral(neutral + 1)} />
+      <Button text="Bad" fonction={() => setBad(bad + 1)} />
+      <h2>Statistics</h2>
+      <Statictics good={good} neutral={neutral} bad={bad} />
     </div>
   )
 }
-
 export default App
