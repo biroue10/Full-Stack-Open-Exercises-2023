6587cf36a0db0bdd85f5c93ad150afc6a358876c
biroue isaac <biroueisaac@gmail.com>
2023-05-21 11:05:26 +0100

    refactor my code

--- part04/index.js
+++ part04/index.js
@@ -12,7 +12,7 @@ const blogSchema = new mongoose.Schema({
 
 const Blog = mongoose.model('Blog', blogSchema)
 
-const mongoUrl = 'mongodb://localhost/bloglist'
+const mongoUrl = 'mongodb+srv://biroue10:yRkkjP3F1jOxoyjh@cluster0.z8qsr8z.mongodb.net/Bloglist?retryWrites=true&w=majority'
 mongoose.connect(mongoUrl)
 
 app.use(cors())
