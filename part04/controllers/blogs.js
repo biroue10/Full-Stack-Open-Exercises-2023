8eec6265916164d9a2de3f0c174671086e03a818
biroue isaac <biroueisaac@gmail.com>
2023-05-21 13:00:33 +0100

    Verfy First Commit

---
+++ part04/controllers/blogs.js
@@ -0,0 +1,26 @@
+const blogsRouter = require('express').Router()
+const Blog = require('../models/blog')
+
+blogsRouter.get('/api/blogs', (request, response) => {
+    Blog.find({}).then(blogs => {
+        response.json(blogs)
+    })
+})
+
+blogsRouter.post('/', (request, response, next) => {
+    const body = request.body
+
+    const blog = new Blog({
+        content: body.content,
+        important: body.important || false,
+        date: new Date()
+    })
+
+    blog.save()
+        .then(savedBlog => {
+            response.json(savedBlog)
+        })
+        .catch(error => next(error))
+})
+
+module.exports = blogsRouter
