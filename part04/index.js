971e11cfc7afb06b86951fa78b19802325ba145a
biroue isaac <biroueisaac@gmail.com>
2023-05-21 11:33:47 +0100

    Refactor my code

--- part04/index.js
+++ part04/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
+const logger = require('./utils/logger')
 
 const blogSchema = new mongoose.Schema({
   title: String,
@@ -11,13 +12,10 @@ const blogSchema = new mongoose.Schema({
 })
 
 const Blog = mongoose.model('Blog', blogSchema)
-
 const mongoUrl = 'mongodb+srv://biroue10:307DKVFKpGjctmdz@cluster0.z8qsr8z.mongodb.net/blog2?retryWrites=true&w=majority'
 mongoose.connect(mongoUrl)
-
 app.use(cors())
 app.use(express.json())
-
 app.get('/api/blogs', (request, response) => {
   Blog
     .find({})
@@ -25,18 +23,15 @@ app.get('/api/blogs', (request, response) => {
       response.json(blogs)
     })
 })
-
 app.post('/api/blogs', (request, response) => {
   const blog = new Blog(request.body)
-
   blog
     .save()
     .then(result => {
       response.status(201).json(result)
     })
 })
-
 const PORT = 3003
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+app.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
 })
